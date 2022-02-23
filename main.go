aac95ce2c8a017067fde08e83952c621182db3dc
XmchxCoder <1394466835@qq.com>
2022-02-23 13:57:28 +0800

    重构

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 
 func main() {
 	e := engine.ConcurrentEngine{
-		Scheduler:   &scheduler.SimpleScheduler{},
+		Scheduler:   &scheduler.QueuedScheduler{},
 		WorkerCount: 100,
 	}
 
 	e.Run(engine.Request{
-		Url:        "/tags/言情?page=6",
-		ParserFunc: parser.ParseTag,
+		Url:        "/hot-tags",
+		ParserFunc: parser.ParseTagList,
 	})
 }
--- scheduler/simple.go
+++ scheduler/simple.go
@@ -8,10 +8,17 @@ type SimpleScheduler struct {
 	workerChan chan engine.Request
 }
 
-func (s *SimpleScheduler) Submit(request engine.Request) {
-	go func() { s.workerChan <- request }()
+func (s *SimpleScheduler) WorkerChan() chan engine.Request {
+	return s.workerChan
 }
 
-func (s *SimpleScheduler) ConfigureMasterWorkerChan(c chan engine.Request) {
-	s.workerChan = c
+func (s *SimpleScheduler) WorkerReady(requests chan engine.Request) {
+}
+
+func (s *SimpleScheduler) Run() {
+	s.workerChan = make(chan engine.Request)
+}
+
+func (s *SimpleScheduler) Submit(request engine.Request) {
+	go func() { s.workerChan <- request }()
 }
